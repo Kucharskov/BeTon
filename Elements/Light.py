c560d616a57c9450a8fe2947400a94f94897b397
Michał Kucharski <M.Kucharskov@gmail.com>
2017-07-19 11:45:23 +0200

    Dodanie modułu oświetlenia
    W przyszłości trzeba go przenieść do BeTon.py, jak radar

---
+++ Elements/Light.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import time
+import RPi.GPIO as GPIO
+
+'''BeTon light module'''
+__author__ = "M. Kucharskov"
+__version__ = "1.0.0"
+
+class Light:
+	def __init__(self, pin):
+		self.pin = pin
+	
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(self.pin, GPIO.OUT, initial=GPIO.LOW)
+
+	def turnOn(self):
+		GPIO.output(self.pin, GPIO.HIGH)
+
+	def turnOff(self):
+		GPIO.output(self.pin, GPIO.LOW)
+		
+	def blink(self, times = 1):
+		for i in range(times):
+			self.turnOn()
+			time.sleep(.15)
+			self.turnOff()
+			time.sleep(.15)
--- Test.py
+++ Test.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import time
 from BeTon import BeTon
+from Elements.Light import Light
 
 if __name__ == "__main__":
 	try:
 		bot = BeTon(37, 35, 38, 40, 6.3)
 		bot.setupRadar(33, 36)
+		
+		l = Light(11)
+		l.blink(5)
 		
 		while True:
 			bot.automove(True)
